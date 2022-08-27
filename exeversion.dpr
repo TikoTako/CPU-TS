57086199f7db3838c355ff052b4b0b82c3dd2315
TikoTako <49921594+TikoTako@users.noreply.github.com>
2022-08-27 22:59:04 +0200

    commit

--- exeversion.dpr
+++ exeversion.dpr
@@ -31,24 +31,24 @@ const
   GUID_PROCESSOR_THROTTLE_MINIMUM: TGuid = '{893dee8e-2bef-41e0-89c6-b55d0929964c}';
 
 begin
-// i did this because i need only min and max set to 1 or 100
-// if you need to set AC/DC and min/max at different values you should use
-// the paramstr values
-// something like
-// exeversion.exe -ACmin 50 -ACmax 100 -DCmin 25 -DCmax 50
-// then
-// check paramcount mod 2
-// start with i=1
-// check paramstr(i) = one of -ACmin..-DCmax then the paramstr(i+1) to get the value
-// inc i by 2, check if hit paramcount if not repeat
+  // i did this because i need only min and max set to 1 or 100
+  // if you need to set AC/DC and min/max at different values you should use
+  // the paramstr values
+  // something like
+  // exeversion.exe -ACmin 50 -ACmax 100 -DCmin 25 -DCmax 50
+  // then
+  // check paramcount mod 2
+  // start with i=1
+  // check paramstr(i) = one of -ACmin..-DCmax then the paramstr(i+1) to get the value
+  // inc i by 2, check if hit paramcount if not repeat
   if paramcount < 1 then
   begin
-  writeln('Settin MaX');
+    writeln('Settin MaX');
     vValueW := 100
   end
   else
   begin
-  writeln('SETTIN MIN');
+    writeln('SETTIN MIN');
     vValueW := 1;
   end;
 
@@ -58,13 +58,13 @@ begin
       if PrintR('Get', PowerGetActiveScheme(0, vPGuid)) = 0 then
         try
           PrintR('Read AC min', PowerReadACValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MINIMUM, vValueR));
-          writeln('> ', vValueR);
+          writeln('Current value > ', vValueR);
           PrintR('Read AC max', PowerReadACValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MAXIMUM, vValueR));
-          writeln('> ', vValueR);
+          writeln('Current value > ', vValueR);
           PrintR('Read DC min', PowerReadDCValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MINIMUM, vValueR));
-          writeln('> ', vValueR);
+          writeln('Current value > ', vValueR);
           PrintR('Read DC max', PowerReadDCValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MAXIMUM, vValueR));
-          writeln('> ', vValueR);
+          writeln('Current value > ', vValueR);
 
           PrintR('Write AC min', PowerWriteACValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MINIMUM, vValueW));
           PrintR('Write AC max', PowerWriteACValueIndex(0, vPGuid, @GUID_PROCESSOR_SETTINGS_SUBGROUP, @GUID_PROCESSOR_THROTTLE_MAXIMUM, vValueW));
